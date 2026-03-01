9de740062b1c4e0513a775f110db59c44bd9ebb0
Zxtern <romanjael1@outlook.com>
2026-02-28 22:44:04 -0800

    edited background colors, images, moved and organized files, created a new page...

--- src/About.js
+++ src/About.js
@@ -1,22 +1,21 @@
 import React from "react";
 
+//This is the About component, which serves as the introduction page for the student portfolio. 
+// It includes a welcome message, a self-portrait image, and a brief biography of the student. 
+// The component is styled using CSS classes defined in the App.css file.
+
 function About() {
     return (
         <div className="about-container">
             <div className="about-text">
-                <h2>Welcome to</h2>
-                <h1>Jael Roman's Student Portfolio</h1>
+                <h1>Jael Roman</h1>
+                <h2>Computer Science Student</h2>
+                <h2>Frontend Developer</h2>
                 
             </div>
-            <img src="/self-icon-1.png" alt="self portrait" width="400" height="600"></img>
-            <div className="about-bio">
-                <p>
-                    Hello fellow students, instructors, and visitors. My name is Jael Roman and I am a junior enrolled at California State University of Monterey Bay.
-                    I am an aspiring software engineer and currently pursuing a Bachelors in Computer Science. 
-                </p>
-
+            <div className="self-img">
+                <img src="/self-icon-1.png" alt="self portrait" width="400" height="600"></img>
             </div>
-
         </div>
     );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import About from './About.js';
 import Courses from './Courses.js';
+import AboutMe from './About-Me.js';
 function Home() {
     return (
         <>
            <About />
+           <AboutMe />
            <Courses />
         </>
     );
